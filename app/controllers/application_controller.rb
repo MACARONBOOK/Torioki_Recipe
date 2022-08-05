b2b9002c731a744ef70d6125493fd3bd410d592e
MACARONBOOK <106646758+MACARONBOOK@users.noreply.github.com>
2022-08-05 03:39:28 +0000

    creating 新規登録/ログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :authenticate_customer!, unless: :admin_url
+  before_action :authenticate_user!, unless: :admin_url
   before_action :authenticate_admin!, if: :admin_url
   before_action :configure_permitted_parameters, if: :devise_controller?
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,46 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
+
+  devise_for :admins, controllers: {
+    sessions:      "admins/sessions",
+    passwords:     "admins/passwords",
+    registrations: "admins/registrations"
+}
+
+  devise_for :users, controllers: {
+    sessions:      "users/sessions",
+    passwords:     "users/passwords",
+    registrations: "users/registrations"
+}
+
+  # ルーティングエラー対策(試し)
+  get "admins" => "admins/users#admins"
+
+  # ゲストログイン
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
-  namespace :public do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
+
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => "homes#about"
+    get "/search" => "homes#search"
+
+    patch ":id/withdraw/:name" => "homes#withdraw", as: "withdraw_user"
+    put "withdraw/:name" => "users#withdraw"
+
+    resources :users, only: [:show, :index, :edit, :update] do
+      member do
+          get :following, :followers
+      end
+    end
+    resource :relationships, only: [:create, :destroy]
   end
+
+
   namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
+    resources :users, except: [:new, :create]
   end
-  devise_for :admins
-  devise_for :users
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
