7c5df401d09249607219a82a51f38fd3e2f8655c
MACARONBOOK <106646758+MACARONBOOK@users.noreply.github.com>
2022-08-12 02:52:56 +0000

    check error-movement

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -5,20 +5,20 @@ class Public::CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     @comment_new = Comment.new
     @comment.recipe_id = @recipe.id
-    if comment.save
+    if @comment.save
       # if comment.reply_comment.nil?
         flash[:notice] = 'レシピへのコメントを投稿しました。'
       # else
       #   flash[:notice] = 'コメントに返信しました。'
       # end
-      redirect_to comment.recipe
+      redirect_to @comment.recipe
     else
       # if comment.reply_comment.nil?
       #   redirect_to request.referer, flash[:comment] = comment
       # end
 
-      flash[:error_messages] = comment.errors.full_messages
-      redirect_back fallback_location: comment.recipe
+      flash[:error_messages] = @comment.errors.full_messages
+      redirect_back fallback_location: @comment.recipe
     end
   end
 
@@ -35,6 +35,6 @@ class Public::CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:user_id, :recipe_id, :name, :content)
+    params.require(:comment).permit(:user_id, :recipe_id, :name, :content, :rate)
   end
 end
