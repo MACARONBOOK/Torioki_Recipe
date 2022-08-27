5a442d6ecc4c4a5f0b46533719a5f72ea7c22896
MACARONBOOK <106646758+MACARONBOOK@users.noreply.github.com>
2022-08-27 16:55:04 +0000

    change img-at-view

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -28,11 +28,11 @@ class Recipe < ApplicationRecord
   end
 
   def get_image(width, height)
-    unless image.attached?
+    unless images.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      images.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    images
   end
 
 
